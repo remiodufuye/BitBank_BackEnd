72bfd1d550dd79f60e36be0ac8b1a981876a7c2d
github username <github email address>
2020-02-04 19:37:04 -0500

    changes made

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,31 @@
 class UsersController < ApplicationController
+
+    def index
+        users = User.all 
+        render json: users.to_json(serialize_data)
+    end 
+
+    
+    def show
+        user = User.find(params[:id])
+        render json: user.to_json(serialize_data)
+    end
+   
+    private 
+
+    def serialize_data
+        {:except => [:created_at,:updated_at]}
+    end 
+    
+    # from ComicCon 
+    # def serialize_data
+    #     {:except => [:created_at,:updated_at],
+    #         :include => {
+    #             :movies => 
+    #             {
+    #                 :except => [:created_at, :updated_at]
+    #             }
+    #         }}
+    # end 
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :watchitems
   resources :users
   resources :currencies
+  post '/login', to: 'auth#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
