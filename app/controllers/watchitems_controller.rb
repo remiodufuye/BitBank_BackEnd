19e42d4353808fd32681e89533091b883792fc98
github username <github email address>
2020-02-07 12:56:14 -0500

    add watchlist feature

--- app/controllers/watchitems_controller.rb
+++ app/controllers/watchitems_controller.rb
@@ -7,13 +7,16 @@ class WatchitemsController < ApplicationController
 
 
     def create 
+        # currency = Currency.find_by(coin_id: 5)
+        # currency = Currency.find_by(params[:coin_id])
         watchitem = Watchitem.create(strong_params)
+        # debugger
         if watchitem.valid? 
         render json: {message: "Coin added to watchlist!", watchitem: watchitem.to_json(serialized_data)}
          else 
-        render json: { message: "Coin already added to watchlist"}
+        render json: {message: "Coin already added to watchlist"}
          end
-    end 
+    end  
 
 
     private 
