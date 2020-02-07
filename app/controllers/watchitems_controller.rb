870b02f2f9902c0b94bd491ba8906e4f1770d0e6
github username <github email address>
2020-02-07 14:17:41 -0500

    changes made

--- app/controllers/watchitems_controller.rb
+++ app/controllers/watchitems_controller.rb
@@ -7,14 +7,17 @@ class WatchitemsController < ApplicationController
 
 
     def create 
-        # currency = Currency.find_by(coin_id: 5)
-        # currency = Currency.find_by(params[:coin_id])
-        # Watchitem.create(user_id:params[:id] , currency_id: currency)   -- from lbs 
+       
+        coin = Currency.find_by(coin_id: params[:coin_id])
+        watchitem = Watchitem.create(user_id:params[:user_id] , currency_id: coin.id)   
 
-        watchitem = Watchitem.create(strong_params)
+        # Currency.find_by(coin_id: 5)
+        # Watchitem.create(user_id:37 , currency_id: 105) 
+
+        # watchitem = Watchitem.create(strong_params) 
         
         if watchitem.valid? 
-        debugger
+        byebug
         render json: {message: "Coin added to watchlist!", watchitem: watchitem.to_json(serialized_data)}
          else 
         render json: {message: "Coin already added to watchlist"}
@@ -32,10 +35,6 @@ class WatchitemsController < ApplicationController
         {:except => [:created_at , :updated_at]}
     end 
 
-
-    # name = Faker::Name.first_name
-    # species = Faker::Games::Pokemon.name
-    # Pokemon.create(nickname: name, species: species, trainer_id: trainer.id)
 
 end
 
