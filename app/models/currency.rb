d02f2ed65a9943b4b6695ea331bd3fa27816070d
github username <github email address>
2020-02-10 15:41:35 -0500

    changes made

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,7 +1,7 @@
 class Currency < ApplicationRecord
     has_many :watchitems
     has_many :users , through: :watchitems
-    # has_many :portfolios
-    # has_many :users , through: :portfolios
+    has_many :portfolios
+    has_many :users , through: :portfolios
 end
  
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
     has_many :watchitems
     has_many :currencies , through: :watchitems
-    # has_many :portfolios 
-    # has_many :currencies , through: :portfolios
+    has_many :portfolios 
+    has_many :currencies , through: :portfolios
     has_secure_password
     validates :username , uniqueness: { case_sensitive: false }
 end
