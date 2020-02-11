9759a055ac0c0f2b43770830ef6a68d745cdc595
github username <github email address>
2020-02-11 12:23:01 -0500

    portfolio feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -5,14 +5,23 @@ class PortfoliosController < ApplicationController
         render json: portfolio.to_json(serialized_data)
     end 
 
+    def create
+        coin = Currency.find_by(coin_id: params[:currency_id])
+        portfolioitem = Portfolio.create(user_id:params[:user_id] , currency_id: coin.id ,  amount: params[:amount] , 
+                     value: params[:value])
+
+                 if portfolioitem.valid? 
+                 render json: {message: "Coin added to Your Portfolio!", portfolio: portfolioitem.to_json(serialized_data)}
+                 else 
+                 render json: {message: "Coin already added to watchlist"}
+                 end
+    end 
+    
     private
 
     def serialized_data
-        {:except => [:created_at,:updated_at]}
+        {:except => [:created_at , :updated_at], :include => [currency: {:except => [:created_at , :updated_at] }]}
     end 
 
-
- 
 end
-
 
