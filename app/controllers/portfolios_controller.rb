6eb7b07c74e6952e8d3d3c1993271cf71741a973
github username <github email address>
2020-02-12 05:52:26 -0500

    update portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,9 +1,12 @@
 class PortfoliosController < ApplicationController
 
+    before_action :get_currency ,only:[:update]
+
     def index
         portfolio = Portfolio.all 
         render json: portfolio.to_json(serialized_data)
     end 
+
 
     def create
         coin = Currency.find_by(coin_id: params[:currency_id])
@@ -13,15 +16,50 @@ class PortfoliosController < ApplicationController
                  if portfolioitem.valid? 
                  render json: {message: "Coin added to Your Portfolio!", portfolio: portfolioitem.to_json(serialized_data)}
                  else 
-                 render json: {message: "Coin already added to watchlist"}
+                 render json: {message: "Something Went Wrong !! "}
                  end
+                 
     end 
+
+
+    def update
+     
+        coin = Currency.find_by(coin_id: params[:currency_id]) 
+        # newPortfolioItem = Portfolio.update( amount: params[:amount] , value: params[:value])
+        newPortfolioItem = coin.update( amount: params[:amount] , value: params[:value])
+
+        if newPortfolioItem.valid? 
+            render json: {message: "Portfolio Updated!!", portfolio: newPortfolioItem.to_json(serialized_data)}
+            else 
+            render json: {message: "Something Went Wrong !! "}
+            end
+
+    end
+
     
     private
+    
+    def get_currency
+        @curency = Currency.find(params[:id])
+    end 
 
     def serialized_data
         {:except => [:created_at , :updated_at], :include => [currency: {:except => [:created_at , :updated_at] }]}
     end 
 
+    def strong_params
+        params.require(:portfolio).permit(:user_id , :currency_id , :amount , :value )  
+    end
+
 end
 
+
+# def update
+#     @course = @review.course
+#     @review.update(strong_params)
+#     if @review.valid?
+#         redirect_to user_path(@review.user)
+#     else
+#         render :edit
+#     end
+# end
