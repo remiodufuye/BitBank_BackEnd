42c2970187e4e7f75a0122558a08249574968863
github username <github email address>
2020-02-13 10:53:42 -0500

    changes made

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,22 +10,25 @@ class UsersController < ApplicationController
         user = User.find(params[:id])
         render json: user.to_json(serialize_data)
     end
+
+    def create
+        # byebug
+        user = User.create(username: params[:user][:username] , password: params[:user][:password])
+        render json: user.to_json(serialize_data)
+
+        if user.valid? 
+            render json: {message: "New User Created", user: user.to_json(serialized_data)}
+            else 
+            render json: {message: "Something Went Wrong !! "}
+            end
+    
+    end 
    
     private 
 
     def serialize_data
         {:except => [:created_at,:updated_at]}
     end 
-    
-    # from ComicCon 
-    # def serialize_data
-    #     {:except => [:created_at,:updated_at],
-    #         :include => {
-    #             :movies => 
-    #             {
-    #                 :except => [:created_at, :updated_at]
-    #             }
-    #         }}
-    # end 
+
   
 end
